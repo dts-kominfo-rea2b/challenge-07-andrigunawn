997d5751213560f6604c3b383b7825b1b7619ab9
andrigunawn <andri.gnwan@gmail.com>
2022-06-12 23:34:30 +0700

    first commit promise

--- promise.js
+++ promise.js
@@ -1,7 +1,28 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const hitung = (data, result) =>{
+  let total =  0;
+  data.forEach(film => {
+    if(film.hasil == result){
+      total++;
+    }
+    
+  });
+  return total;
+}
+
+const promiseOutput = (result) =>{
+  return new Promise((jum) =>{
+    promiseTheaterIXX().then((IXX)=>{
+      let theaterIXX = hitung(IXX, result);
+      promiseTheaterVGC().then((VGC)=>{
+        let theaterVGC = hitung(VGC, result);
+        jum(theaterIXX + theaterVGC)
+      })
+    })
+  });
+};
 
 module.exports = {
   promiseOutput,
